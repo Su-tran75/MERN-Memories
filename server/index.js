2cf670cdaa2f2088677c5a3e9f9eccfba5ff00fa
su-tran75 <su-tran@hotmail.fr>
2022-03-23 02:08:08 +0800

    connect express application to localhost 5500

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,7 +15,7 @@ app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://su:Mongodbav6pyn8h@memories.lglql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
